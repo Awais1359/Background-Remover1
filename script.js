a6a26424525b7fd073c1a769069c77b8cb9ef53b
Awais1359 <itsawais2213406@gmail.com>
2025-06-11 15:49:38 +0500

    project 1

---
+++ script.js
@@ -0,0 +1,30 @@
+// alert
+//  ("Thanks for your input!");
+//  var name = "Mark"
+// document.write ("Hello")
+
+// var names = "Awais";
+//  names = "ali"
+// console.log (names)
+// var weight = 150;
+// weight + 25
+
+// var plu = weight + 20
+// console.log(plu);
+// document.write(plu);
+
+// let a = 1
+// a=++
+// console.log(a);
+
+
+const a = "10"
+ b = null
+
+
+console.log(typeof(b));
+
+
+
+
+
